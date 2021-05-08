1a86bb9b7319d3f61bad63076d20d668f4ca8f46
sunwenqi@51888.cn <78629322>
2021-05-08 17:53:49 +0800

    添加eslint规则

--- public/index/index.js
+++ public/index/index.js
@@ -1,14 +1,14 @@
-import "./style/reset.css";
-import "./style/common.css";
-import "lib-flexible/flexible";
-import Vue from "vue";
-import App from "./App";
-import router from "./page/router";
+import './style/reset.css'
+import './style/common.css'
+import 'lib-flexible/flexible'
+import Vue from 'vue'
+import App from './App'
+import router from './page/router'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  el: "#app",
-  router,
-  render: (h) => h(App),
-});
+	el: '#app',
+	router,
+	render: (h) => h(App)
+})
--- public/index/page/visualized.vue
+++ public/index/page/visualized.vue
@@ -1,169 +1,169 @@
 <!--  -->
 <template>
-  <div ref="canvas" class="canvas"></div>
+  <div ref="canvas" class="canvas" />
 </template>
 
 <script>
-import * as echarts from "echarts/core";
-import * as api from "../script/api";
-import dayjs from "dayjs";
+import * as echarts from 'echarts/core'
+import * as api from '../script/api'
+import dayjs from 'dayjs'
 import {
-  ToolboxComponent,
-  TooltipComponent,
-  GridComponent,
-  DataZoomComponent,
-  LegendComponent,
-} from "echarts/components";
-import { LineChart } from "echarts/charts";
-import { CanvasRenderer } from "echarts/renderers";
+	ToolboxComponent,
+	TooltipComponent,
+	GridComponent,
+	DataZoomComponent,
+	LegendComponent
+} from 'echarts/components'
+import { LineChart } from 'echarts/charts'
+import { CanvasRenderer } from 'echarts/renderers'
 echarts.use([
-  ToolboxComponent,
-  TooltipComponent,
-  GridComponent,
-  DataZoomComponent,
-  LineChart,
-  CanvasRenderer,
-  LegendComponent,
-]);
+	ToolboxComponent,
+	TooltipComponent,
+	GridComponent,
+	DataZoomComponent,
+	LineChart,
+	CanvasRenderer,
+	LegendComponent
+])
 const weekTxt = {
-  0: "周日",
-  1: "周一",
-  2: "周二",
-  3: "周三",
-  4: "周四",
-  5: "周五",
-  6: "周六",
-};
+	0: '周日',
+	1: '周一',
+	2: '周二',
+	3: '周三',
+	4: '周四',
+	5: '周五',
+	6: '周六'
+}
 const color = [
-  "#1890FF",
-  "#13C2C2",
-  "#2FC25B",
-  "#FACC14",
-  "#F04864",
-  "#8543E0",
-  "#3436C7",
-  "#223273",
-];
+	'#1890FF',
+	'#13C2C2',
+	'#2FC25B',
+	'#FACC14',
+	'#F04864',
+	'#8543E0',
+	'#3436C7',
+	'#223273'
+]
 export default {
-  data() {
-    return {
-      lastData: {},
-      weeks: [],
-    };
-  },
 
-  components: {},
+	components: {},
+	data() {
+		return {
+			lastData: {},
+			weeks: []
+		}
+	},
 
-  computed: {},
+	computed: {},
+	watch: {
+		$route(val) {
+			this.handle()
+		}
+	},
 
-  mounted() {
-    this.handle();
-  },
-  watch: {
-    $route(val) {
-      this.handle();
-    },
-  },
-  methods: {
-    handle() {
-      this.weeks = Array.from({ length: 7 }, (item, i) => ({
-        name: weekTxt[i],
-        type: "line",
-        symbolSize: 3,
-        symbol: "circle",
-        sampling: "lttb",
-        itemStyle: {
-          color: color[i],
-        },
-        lineStyle: {
-          width: 1,
-          color: color[i],
-        },
-        data: [],
-      }));
-      this.getThsFundHistory();
-    },
-    async getThsFundHistory() {
-      const { fundCode } = this.$route.params;
-      const data = await api.getThsFundHistory({ code: fundCode });
-      data.reverse().map((item) => {
-        const getDay = new Date(item.date).getDay();
-        const curWeek = this.weeks[getDay];
-        curWeek.data.push([item.date, +item.accNet]);
-      });
-      this.lastData = data[data.length - 1];
-      this.getThsFundInfo();
-    },
-    async getThsFundInfo() {
-      const { fundCode } = this.$route.params;
-      const data = await api.getTTFundInfo(fundCode);
-      const arr = data.split(",");
-      const today = dayjs(arr[7]).format("YYYY/MM/DD");
-      const lastDay = dayjs(this.lastData.date).get("day");
-      const yesterday = this.weeks[lastDay].data;
-      const lastData = yesterday[yesterday.length - 1];
-      if (data && +new Date(today) > +new Date(lastData[0])) {
-        let lastAcc = lastData[1];
-        const getDay = dayjs(today).get("day");
-        const week = this.weeks[getDay].data;
-        week.push([today, lastAcc + lastAcc * (arr[6] / 100)]);
-      }
-      this.draw();
-    },
-    draw() {
-      const { canvas } = this.$refs;
-      var myChart = echarts.init(canvas);
-      myChart.clear();
-      const option = {
-        tooltip: {
-          trigger: "axis",
-        },
-        legend: {
-          top: "10",
-          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-        },
-        xAxis: {
-          type: "time",
-          axisLabel: {
-            color: "#333",
-            formatter: (params) => dayjs(params).format("YYYY/MM/DD"),
-          },
-          splitLine: {
-            show: true,
-          },
-          axisLine: {
-            lineStyle: {
-              color: "#e0e6f1",
-            },
-          },
-        },
-        yAxis: {
-          min: "dataMin",
-          max: "dataMax",
-        },
-        toolbox: {
-          show: false,
-          feature: {
-            dataZoom: {
-              yAxisIndex: "none",
-            },
-          },
-        },
-        dataZoom: [
-          {
-            start: 85,
-            show: false, //是否显示
-          },
-          {
-            type: "inside",
-          },
-        ],
-        series: this.weeks,
-      };
-      myChart.setOption(option);
-    },
-  },
-};
+	mounted() {
+		this.handle()
+	},
+	methods: {
+		handle() {
+			this.weeks = Array.from({ length: 7 }, (item, i) => ({
+				name: weekTxt[i],
+				type: 'line',
+				symbolSize: 3,
+				symbol: 'circle',
+				sampling: 'lttb',
+				itemStyle: {
+					color: color[i]
+				},
+				lineStyle: {
+					width: 1,
+					color: color[i]
+				},
+				data: []
+			}))
+			this.getThsFundHistory()
+		},
+		async getThsFundHistory() {
+			const { fundCode } = this.$route.params
+			const data = await api.getThsFundHistory({ code: fundCode })
+			data.reverse().map((item) => {
+				const getDay = new Date(item.date).getDay()
+				const curWeek = this.weeks[getDay]
+				curWeek.data.push([item.date, +item.accNet])
+			})
+			this.lastData = data[data.length - 1]
+			this.getThsFundInfo()
+		},
+		async getThsFundInfo() {
+			const { fundCode } = this.$route.params
+			const data = await api.getTTFundInfo(fundCode)
+			const arr = data.split(',')
+			const today = dayjs(arr[7]).format('YYYY/MM/DD')
+			const lastDay = dayjs(this.lastData.date).get('day')
+			const yesterday = this.weeks[lastDay].data
+			const lastData = yesterday[yesterday.length - 1]
+			if (data && +new Date(today) > +new Date(lastData[0])) {
+				const lastAcc = lastData[1]
+				const getDay = dayjs(today).get('day')
+				const week = this.weeks[getDay].data
+				week.push([today, lastAcc + lastAcc * (arr[6] / 100)])
+			}
+			this.draw()
+		},
+		draw() {
+			const { canvas } = this.$refs
+			var myChart = echarts.init(canvas)
+			myChart.clear()
+			const option = {
+				tooltip: {
+					trigger: 'axis'
+				},
+				legend: {
+					top: '10',
+					data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+				},
+				xAxis: {
+					type: 'time',
+					axisLabel: {
+						color: '#333',
+						formatter: (params) => dayjs(params).format('YYYY/MM/DD')
+					},
+					splitLine: {
+						show: true
+					},
+					axisLine: {
+						lineStyle: {
+							color: '#e0e6f1'
+						}
+					}
+				},
+				yAxis: {
+					min: 'dataMin',
+					max: 'dataMax'
+				},
+				toolbox: {
+					show: false,
+					feature: {
+						dataZoom: {
+							yAxisIndex: 'none'
+						}
+					}
+				},
+				dataZoom: [
+					{
+						start: 85,
+						show: false // 是否显示
+					},
+					{
+						type: 'inside'
+					}
+				],
+				series: this.weeks
+			}
+			myChart.setOption(option)
+		}
+	}
+}
 </script>
 <style lang="scss" scoped>
 .canvas {
